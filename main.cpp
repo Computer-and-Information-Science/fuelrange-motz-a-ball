ac8cf284c307c941f37bd039290873ae22e4638a
motz-a-ball <memaza98@gmail.com>
2025-09-09 22:54:38 +0000

    Completed Fuel Range Calculator

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 // Assignment 1 - Fuel Range
-// TODO: YOUR NAME HERE
+// TODO: Matthew Maza
 #include <iostream>
 // TODO: Add the "using" statement
-
+using namespace std;
 int main() {
   // TODO: Declare your variables, with comments for each
+  //capacity of the fuel tank in gallons
+  double capacity;
 
-  // TODO: Prompt for and input the required values
+  //miles per gallon
+  double mpg;
+  
+  //percentage filled
+  int percentFull;
 
+  // TODO: Prompt for and input the required values+
+  cout << "Please input capacity of your fuel tank: ";
+  cin >> capacity;
+  cout << "Please input the mpg of your car: ";
+  cin >> mpg;
+  cout << "Please enter the percent full: ";
+  cin >> percentFull;
   // TODO: Calculate the results
+  
+  double remainingFuel = (percentFull*capacity)/100;
+  double milesLeft = remainingFuel*mpg;
+
+  cout << " The vehicle can be driven " << milesLeft << " miles on the remaining fuel. " << endl;
+
 
   // TODO: Output the results
   
